5c3d6895b306aa4a7dbe034a250a0312c9a6c2bc
ealberto <ealberto@me.com>
2020-04-14 22:40:11 -0400

    access VMS api

--- src/components/home/Login.vue
+++ src/components/home/Login.vue
@@ -1,13 +1,13 @@
 <template>
      <b-container>
         <h1>Sign In</h1>
-        <b-form>
+        <b-form @submit.prevent="login">
             <b-row>
               <b-col class="col-lg-6">
                 <b-form-group
-                  label="Username"
+                  label="Email"
                 >
-                <b-form-input id="username"></b-form-input>
+                <b-form-input id="email"></b-form-input>
                 </b-form-group>
               </b-col>
             </b-row>
@@ -22,7 +22,7 @@
             </b-row>
             <b-row>
               <b-col class="col-lg-6">
-                <b-button>Register</b-button>
+                <b-button type="submit">Register</b-button>
               </b-col>
             </b-row>
         </b-form>
@@ -32,8 +32,35 @@
 </template>
 
 <script>
-export default {
+import router from '../../router'
+import axios from 'axios'
 
+export default {
+  name: 'Login',
+  methods: {
+    login: (e) => {
+      e.preventDefault()
+      let email = e.target.elements.email.value
+      let password = e.target.elements.password.value
+
+      let login = () => {
+        let data = {
+          email: email,
+          password: password
+        }
+
+        axios.post('http://localhost:5000/api/v1/auth/login', data)
+          .then((response) => {
+            console.log('Logged in')
+            router.push('/dashboard')
+          })
+          .catch((errors) => {
+            console.log('Cannot log in')
+          })
+      }
+      login()
+    }
+  }
 }
 </script>
 

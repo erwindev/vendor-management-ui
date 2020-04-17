45ef0db342ab39625efb9a746d86842d6599b00b
ealberto <ealberto@me.com>
2020-04-16 23:02:23 -0400

    re-arrange navbar

--- src/components/shared/Navbar.vue
+++ src/components/shared/Navbar.vue
@@ -4,7 +4,7 @@
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
             <b-navbar-nav class="ml-auto">
-                <b-nav-item :to="{name: 'dashboard'}">Dashboard</b-nav-item>            
+                <b-nav-item :to="{name: 'dashboard'}">Dashboard</b-nav-item>
                 <b-nav-item-dropdown right>
                     <template v-slot:button-content>
                         User

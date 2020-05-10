b48f7c85e8dfb5e9d4654bbae0dbdb7c705c8eab
ealberto <ealberto@me.com>
2020-05-10 16:19:53 -0400

    updated url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-export const ATTACHMENT_API_BASE = `${process.env.VUE_APP_API_URL}/a/api/v1`
-export const CONTACT_API_BASE = `${process.env.VUE_APP_API_URL}/c/api/v1`
-export const NOTES_API_BASE = `${process.env.VUE_APP_API_URL}/n/api/v1`
-export const PRODUCT_API_BASE = `${process.env.VUE_APP_API_URL}/p/api/v1`
-export const USER_API_BASE = `${process.env.VUE_APP_API_URL}/u/api/v1`
-export const VENDOR_API_BASE = `${process.env.VUE_APP_API_URL}/v/api/v1`
+export const ATTACHMENT_API_BASE = `${process.env.VUE_APP_API_URL}/a/v1`
+export const CONTACT_API_BASE = `${process.env.VUE_APP_API_URL}/c/v1`
+export const NOTES_API_BASE = `${process.env.VUE_APP_API_URL}/n/v1`
+export const PRODUCT_API_BASE = `${process.env.VUE_APP_API_URL}/p/v1`
+export const USER_API_BASE = `${process.env.VUE_APP_API_URL}/u/v1`
+export const VENDOR_API_BASE = `${process.env.VUE_APP_API_URL}/v/v1`
